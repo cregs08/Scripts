c1810844ad27575bcb1ec5f7d8c86ea1c364863a
cregs08 <cole.regnier@gmail.com>
2022-06-15 15:55:27 +0700

    added the main method

--- Segmentation/check_matching_tiff_to_image_file.py
+++ Segmentation/check_matching_tiff_to_image_file.py
@@ -17,6 +17,7 @@ e.g: Cabernet-Sauvignon_image_0.et
 import re
 import glob
 import os
+import argparse
 
 
 """
@@ -27,7 +28,8 @@ checking format for tiff files
 def check_tiff_file_format(tiff_file, class_name, apeer_class_name):
     rex_pattern = "[0-9]+"
     extension = '.ome.tiff'
-    correct_format = class_name + '-' + rex_pattern + '_' + apeer_class_name + extension
+    dash_or_underscore = '[_-]+'
+    correct_format = class_name + dash_or_underscore + rex_pattern + dash_or_underscore + apeer_class_name + extension
     rex = re.compile(correct_format)
     if rex.match(tiff_file):
         return True
@@ -50,10 +52,10 @@ def get_valid_invalid_tiff_paths(tiff_mask_paths, class_name, apeer_class_name):
     }
     return checked_tiff_paths
 
+
 """
 checking if tiff files have matching image file 
 """
-
 
 def get_digits_from_file(file):
     return (''.join([s for s in file if s.isdigit()]))
@@ -77,10 +79,7 @@ def get_valid_invalid_tiff_to_image_pairs(tiff_paths, image_dir, class_name, ext
         if os.path.exists(image_path):
             valid_image_paths.append(image_path)
         else:
-            image_not_found = f'Could not find matching pair for {tiff_file} in' \
-                f'\n{image_dir}\n' \
-                f'Created image file: {image_filename}'
-            invalid_image_paths.append(image_not_found)
+            invalid_image_paths.append(tiff_path)
     checked_paths = {
         'valid_paths': valid_image_paths,
         'invalid_paths': invalid_image_paths
@@ -105,10 +104,8 @@ def get_valid_invalid_image_to_tiff_pairs(image_paths, tiff_dir, class_name, ape
         if os.path.exists(tiff_path):
             valid_tiff_paths.append(tiff_path)
         else:
-            tiff_not_found = f'\nCould not find matching pair for {image_file} in' \
-                f'\n{tiff_dir}\n' \
-                f'Created tiff file: {tiff_filename}\n'
-            invalid_tiff_paths.append(tiff_not_found)
+
+            invalid_tiff_paths.append(tiff_path)
     checked_paths = {
         'valid_paths': valid_tiff_paths,
         'invalid_paths': invalid_tiff_paths
@@ -134,14 +131,19 @@ def display_total_paths(total_image_paths, total_mask_paths):
         print(f'####\nFound same number of image paths ({total_image_paths})\n'
               f'And tiff paths({total_mask_paths})')
 
-def display_invalid_paths_pairs(checked_paths):
-    invalid_paths = checked_paths['invalid_paths']
 
-    num_invalid_paths = len(invalid_paths)
-    if num_invalid_paths >0:
-        for path in invalid_paths:
-            print(path)
+def display_invalid_paths_pairs(invalid_paths):
+    ui = input('Display invalid paths\n(y/n)')
 
+    while True:
+        if ui == 'y':
+            for path in invalid_paths:
+                tiff_file = os.path.basename(path)
+                error_msg = f'\nCould not find matching pair for {tiff_file}\n'
+                print(error_msg)
+            return
+        if ui =='n':
+            return
 
 def check_tiff_paths(tiff_mask_paths, class_name, apeer_class_name):
     checked_tiff_paths = get_valid_invalid_tiff_paths(tiff_mask_paths, class_name, apeer_class_name)
@@ -154,20 +156,35 @@ def check_tiff_paths(tiff_mask_paths, class_name, apeer_class_name):
         print('####\nAll tiff files formatted correctly!')
 
 
+def remove_paths(invalid_paths):
+    while True:
+        ui = input(f'Remove {len(invalid_paths)} files from the directory: {os.path.dirname(invalid_paths[0])}?'
+                   f'\n(y/n)')
+        if ui == 'y':
+            for path in invalid_paths:
+                os.remove(path)
+            return
+        if ui == 'n':
+            return
+
 
 def check_image_to_tiff_pairs(image_paths, tiff_dir, class_name, apeer_class_name):
     checked_image_to_tiff_pairs = \
         get_valid_invalid_image_to_tiff_pairs(image_paths, tiff_dir, class_name, apeer_class_name)
-    display_invalid_paths_pairs(checked_image_to_tiff_pairs)
-    invalid = checked_image_to_tiff_pairs['invalid_paths']
-    print(f'####\nFound {len(invalid)} invalid pairs for image to tiff')
+    invalid_paths = checked_image_to_tiff_pairs['invalid_paths']
+    print(f'####\nOut of {len(image_paths)} image paths, found {len(invalid_paths)} invalid pairs for image to tiff')
+    if len(invalid_paths)>0:
+        display_invalid_paths_pairs(invalid_paths)
+        remove_paths(invalid_paths)
 
 
 def check_tiff_to_image_pairs(tiff_mask_paths, image_dir, class_name, ext):
     checked_tiff_to_image_pairs = get_valid_invalid_tiff_to_image_pairs(tiff_mask_paths, image_dir, class_name, ext)
-    display_invalid_paths_pairs(checked_tiff_to_image_pairs)
-    invalid = checked_tiff_to_image_pairs['invalid_paths']
-    print(f'####\nFound {len(invalid)} invalid pairs for tiff to image')
+    invalid_paths = checked_tiff_to_image_pairs['invalid_paths']
+    print(f'####\nOut of {len(tiff_mask_paths)} tiff paths, found {len(invalid_paths)} invalid pairs for tiff to image')
+    if len(invalid_paths)>0:
+        display_invalid_paths_pairs(invalid_paths)
+        remove_paths(invalid_paths)
 
 
 def check_tiff_path_format_and_tiff_image_pairs(tiff_dir, image_dir, class_name, apeer_class_name, ext='.jpg'):
@@ -187,20 +204,46 @@ def check_tiff_path_format_and_tiff_image_pairs(tiff_dir, image_dir, class_name,
 
     print('\nDone checking pairs!')
 
-tiff_dir = "/Users/cole/PycharmProjects/UsefulScripts/Wine-Leaves/segementation-a/Orginal-Images-Masks/Masks/Tiff-files/Cabernet-Sauvignon"
-image_dir = "/Users/cole/PycharmProjects/UsefulScripts/Wine-Leaves/segementation-a/Orginal-Images-Masks/Images/Cabernet-Sauvignon"
-image_paths = glob.glob(os.path.join(image_dir, '*.jpg'))
 
-class_name = 'Cabernet-Sauvignon'
-apeer_class_name = 'Cabernet'
-ext = '.jpg'
-check_tiff_path_format_and_tiff_image_pairs(tiff_dir=tiff_dir,
-                                            image_dir=image_dir,
-                                            class_name=class_name,
-                                            apeer_class_name=apeer_class_name)
-# checked = get_valid_invalid_image_to_tiff_pairs(image_paths, tiff_dir, class_name, apeer_class_name)
-# for path in checked['invalid_paths']:
-#     print(path)
-# valid_paths = checked_paths['valid_paths']
-# invalid_paths = checked_paths['invalid_paths']
+def main():
+    parser = argparse.ArgumentParser(
+        description="rename and saving mask files")
 
+    parser.add_argument("-t",
+                        "--TIFFDIR",
+                        help="path to where the masks are stored as .tiff files",
+                        type=str)
+    parser.add_argument("-i",
+                         "--IMAGEDIR",
+                        help="where we save our newly made masks. default will be DIRS['MASK_DIR] + class_name",
+                        type=str)
+    parser.add_argument("-a",
+                        "--APEERCLASSNAME",
+                        type=str)
+    parser.add_argument("-c",
+                        "--CLASSNAME",
+                        help="name of the class. defualt is name of the base dir for TIFFDIR",
+                        nargs='?',
+                        type=str)
+    parser.add_argument("-e",
+                        "--EXT",
+                        help="extentsion for image files ",
+                        default='.jpg',
+                        type=str)
+
+    args = parser.parse_args()
+    tiff_dir = args.TIFFDIR
+    image_dir = args.IMAGEDIR
+    class_name = args.CLASSNAME
+    apeer_class_name = args.APEERCLASSNAME
+    print(args)
+
+    check_tiff_path_format_and_tiff_image_pairs(tiff_dir=tiff_dir,
+                                                image_dir=image_dir,
+                                                class_name=class_name,
+                                                apeer_class_name=apeer_class_name,
+                                                ext=args.EXT)
+
+
+if __name__ == '__main__':
+    main()
